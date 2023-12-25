db5ef03f40e8e173f271ef9abe134c3758f6b021
Marko Popović <63392259+markonije@users.noreply.github.com>
2023-12-25 20:07:20 +0100

    gasfg

--- theme.config.js
+++ theme.config.js
@@ -4,7 +4,7 @@ export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
       <time>{YEAR}</time> © Marko Popović.
-      <a href="/feed.xml">RSS</a>
+      
       <style jsx>{`
         a {
           float: right;
